09c35f64d20efa0fbe033495994315b5e32d1ab3
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-22 11:16:45 +0600

    create spotlight from aceternity

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
-        
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
     </div>
   )
 }
