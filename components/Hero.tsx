1b3e9d7cf1d6f28e8fb7fa79c8a6a8dd3e6a832a
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-22 12:06:42 +0600

    spotlight added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Spotlight } from './ui/Spotlight'
+import { Spotlight } from './ui/spotlight-new'
 
 const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
         <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
       />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Spotlight <br /> which is not overused.
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          A subtle yet effective spotlight effect, because the previous version
+          is used a bit too much these days.
+        </p>
+      </div>
     </div>
   )
 }
