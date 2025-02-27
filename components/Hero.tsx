610aedbae8d456684bc5ff2f60fa97d6fba982f1
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-27 19:24:29 +0600

    flip-words

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,6 +8,7 @@ const Hero = () => {
     <div className="pb-20 pt-36 h-screen">
       <Spotlight />
       <div className="flex flex-col justify-center items-center text-xl">
+        {/* name section  */}
         <div>
           <p className=" tracking-widest">
             Hi, I&apos;m <span className="font-bold">
@@ -15,10 +16,18 @@ const Hero = () => {
             </span>
           </p>
         </div>
+
+        {/* developer */}
         <div className="relative w-full flex justify-center">
           <RevealText></RevealText>
-          <div className="absolute top-0 left-0">web</div>
+          <div className="absolute -top-5 left-0">web</div>
         </div>
+
+        {/* work  */}
+
+        <div className="bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)] text-center">
+            <span className="">Crafting Modern, High-Performance & Scalable <br /> Web Solutions with Next.js & Modern Technologies.</span>
+          </div>
       </div>
     </div>
   );
