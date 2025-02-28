b9d7e8b997cd6a20ca6eb352dabae03b63d0bfe3
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-28 12:54:14 +0600

    hero parallax componants from acertinity

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
         {/* <Hero></Hero> */}
-        <About></About>
+        {/* <About></About> */}
       </div>
     </main>
   );
--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import profile from "@/public/images/parvaj.png";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const About = () => {
+  const text1 = `I'm Md. Parvaj Mosharof, a full-stack web developer skilled in JavaScript, TypeScript, React, Next.js, Node.js, and PostgreSQL. I’m passionate about building scalable web apps and I’ve built projects like Rento (real estate platform), Diagno (Diagnosting Center), and a Library Management System. Currently, I’m pursuing a BSc in CSE at Uttara University and constantly improving my skills. Check out my work and let's connect! 🚀.`;
+
   return (
     <div className="h-screen flex items-center">
       {/* profile image  */}
@@ -15,25 +18,11 @@ const About = () => {
       </div>
       {/* about text  */}
 
-      <p>
-        Hi, I&apos;m Md. Parvaj Mosharof, a passionate full-stack web developer
-        with expertise in JavaScript, TypeScript, React, Next.js, Node.js,
-        Express.js, MongoDB, and PostgreSQL. I love building dynamic and
-        scalable web applications with clean, efficient code.
-        <br />
-        Currently, I&apos;m pursuing a BSc in Computer Science & Engineering at
-        Uttara University, where I actively participate in competitive
-        programming to sharpen my problem-solving skills. I also enjoy exploring
-        cloud computing and am currently learning AWS to expand my expertise in
-        cloud-based solutions.
-        <br />
-        I have worked on multiple projects, including real estate platforms,
-        tour booking applications, and library management systems, integrating
-        modern authentication, database management, and advanced UI/UX design.
-        <br />
-        My goal is to continuously learn, innovate, and contribute to impactful
-        projects while growing as a developer. Feel free to check out my work
-        and connect with me!
+      <p className="w-1/2 text-sm">
+        <TextGenerateEffect words={text1}></TextGenerateEffect>
+        {/* <TextGenerateEffect words={text2}></TextGenerateEffect>
+        <TextGenerateEffect words={text3}></TextGenerateEffect>
+        <TextGenerateEffect words={text4}></TextGenerateEffect> */}
       </p>
     </div>
   );
