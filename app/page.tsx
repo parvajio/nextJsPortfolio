91514362e486bda2c57719efa454d826cb229920
parvajio <mdparvajmosharof17@gmail.com>
2025-03-02 17:37:31 +0600

    checking optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // import About from "@/components/About";
-// import Hero from "@/components/Hero";
+import Hero from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import Skills from "@/components/Skills";
 // import Remote from "@/components/ui/Remote";
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
-        {/* <Hero></Hero> */}
+        <Hero></Hero>
         {/* <About></About> */}
         <Projects></Projects>
         <Skills></Skills>
