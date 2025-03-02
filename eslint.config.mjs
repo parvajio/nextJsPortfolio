bc4c2a55babf3c3b359d841346f17e45facf169b
parvajio <mdparvajmosharof17@gmail.com>
2025-03-02 13:44:22 +0600

    eslint config for type

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Disable "no-explicit-any"
+      "react-hooks/exhaustive-deps": "off", // Disable exhaustive-deps warning
+    },
+  },
 ];
 
 export default eslintConfig;
