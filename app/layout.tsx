e16bf8c5ee9b9223bc95e52cce6a850bd7df26c0
parvajio <mdparvajmosharof17@gmail.com>
2025-03-04 19:47:42 +0600

    lenis smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ReactLenis } from "@/utils/lenis";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ReactLenis root>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
@@ -38,6 +40,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </body>
+      </ReactLenis>
     </html>
   );
 }
