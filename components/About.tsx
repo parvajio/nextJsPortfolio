75eec3e42efd5d2185b116e96fdae81dfaf2488c
parvajio <mdparvajmosharof17@gmail.com>
2025-03-05 15:23:22 +0600

    error fixed for deploy

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ import { AboutScroll } from "./AboutScroll";
 // import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const About = () => {
-  const text1 = `I'm Md. Parvaj Mosharof, a full-stack web developer skilled in JavaScript, TypeScript, React, Next.js, Node.js, and PostgreSQL. I’m passionate about building scalable web apps and I’ve built projects like Rento (real estate platform), Diagno (Diagnosting Center), and a Library Management System. Currently, I’m pursuing a BSc in CSE at Uttara University and constantly improving my skills. Check out my work and let's connect! 🚀.`;
+  // const text1 = `I'm Md. Parvaj Mosharof, a full-stack web developer skilled in JavaScript, TypeScript, React, Next.js, Node.js, and PostgreSQL. I’m passionate about building scalable web apps and I’ve built projects like Rento (real estate platform), Diagno (Diagnosting Center), and a Library Management System. Currently, I’m pursuing a BSc in CSE at Uttara University and constantly improving my skills. Check out my work and let's connect! 🚀.`;
 
   return (
     <div className="relative w-full">
