87a98009815b8ae1c4552079efa52c7fec26badc
parvajio <mdparvajmosharof17@gmail.com>
2025-03-05 21:35:32 +0600

    skills heading with sparkles

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 import { Box, Lock, Search, Settings, Sparkles } from "lucide-react";
 import GridItem from "./GridItem";
-import { backend, frontend} from "@/lib/skillsArray";
+import { backend, frontend } from "@/lib/skillsArray";
+import { SparklesCore } from "./ui/sparkles";
 
 const Skills = () => {
-
-   
-
   return (
     <div className="flex flex-col justify-center mt-20 overflow-hidden max-w-7xl">
-      <h1 className="text-5xl font-bold text-blue-100 text-center mb-2">Skills</h1>
+      <h1 className="text-7xl font-bold text-blue-100 text-center mb-2">
+        Skills
+      </h1>
       {/* gradient */}
-      <div className="w-[40rem] relative mb-10 mx-auto">
+      <div className="w-[40rem] h-[8rem] relative mb-10 mx-auto">
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
@@ -19,14 +19,16 @@ const Skills = () => {
 
         {/* SPARKLES  */}
 
-        {/* <SparklesCore
+        <SparklesCore
           background="transparent"
           minSize={0.4}
           maxSize={1}
           particleDensity={1200}
           className="w-full h-full"
           particleColor="#FFFFFF"
-        /> */}
+        />
+
+        <div className="absolute inset-0 w-full h-full bg-black-100 [mask-image:radial-gradient(300px_100px_at_top,transparent_20%,white)]"></div>
       </div>
 
       {/* cards */}
@@ -41,9 +43,7 @@ const Skills = () => {
 
         <GridItem
           area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
-          icon={
-            <Settings className="h-4 w-4 text-neutral-400" />
-          }
+          icon={<Settings className="h-4 w-4 text-neutral-400" />}
           title="Back-end"
           description="Yes, it's true. I'm not even kidding. Ask my mom if you don't believe me."
           items={backend}
@@ -58,9 +58,7 @@ const Skills = () => {
 
         <GridItem
           area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
-          icon={
-            <Sparkles className="h-4 w-4 text-neutral-400" />
-          }
+          icon={<Sparkles className="h-4 w-4 text-neutral-400" />}
           title="Database"
           description="I'm not even kidding. Ask my mom if you don't believe me."
         />
