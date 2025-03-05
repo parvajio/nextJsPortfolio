9e6453aafc965064c96e993840ee27e77f2701b0
parvajio <mdparvajmosharof17@gmail.com>
2025-03-05 21:06:32 +0600

    profile posistioned

--- components/ui/google-gemini-effect.tsx
+++ components/ui/google-gemini-effect.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { motion, MotionValue } from "framer-motion";
+import Image from "next/image";
 import React from "react";
 
 const transition = {
@@ -10,28 +11,39 @@ const transition = {
 
 export const GoogleGeminiEffect = ({
   pathLengths,
-  title,
-  description,
   className,
 }: {
   pathLengths: MotionValue[];
-  title?: string;
-  description?: string;
   className?: string;
 }) => {
   return (
     <div className={cn("sticky top-80", className)}>
-      <p className="text-lg md:text-7xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
-        {title || `Build with Aceternity UI`}
-      </p>
-      <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
-        {description ||
-          `Scroll this component and see the bottom SVG come to life wow this
-        works!`}
-      </p>
-      <div className="w-full h-[890px] -top-60 md:-top-40  flex items-center justify-center bg-red-transparent absolute ">
-        <button className="font-bold bg-white rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-24 mt-8 z-30 md:text-base text-black text-xs  w-fit mx-auto ">
-          ui.aceternity.com
+      <div className="flex justify-center items-center -top-40 absolute w-full">
+        <div className="relative">
+          <div className="absolute -left-[80%] top-40 w-full text-right">
+            <p>
+            passionate web developer skilled in JavaScript, React, and Next.js, building modern and scalable applications. Currently pursuing BSc in Computer Science.{" "}
+            </p>
+          </div>
+
+          <Image
+            src={"https://ik.imagekit.io/parvajio/my%20photos/wbg2.png"}
+            alt="parvajio"
+            width={500}
+            height={1000}
+            className="brightness-90 flex justify-center items-center"
+          ></Image>
+
+          <div className="absolute -right-[80%] top-40 w-full text-left">
+            <p>
+            I aim to become a full-stack developer while enhancing my problem-solving skills through competitive programming.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[890px] -top-52 md:-top-40 mt-4 flex items-center justify-center bg-red-transparent absolute">
+        <button className="font-bold bg-white rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-24 mt-8 z-30 md:text-base text-black text-xs  w-fit mx-auto">
+          @parvajio
         </button>
       </div>
       <svg
