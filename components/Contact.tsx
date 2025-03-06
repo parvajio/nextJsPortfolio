b74ec1a891a6ca186109f80598fd32b806234fec
parvajio <mdparvajmosharof17@gmail.com>
2025-03-07 00:41:23 +0600

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Profile } from "./Profile";
 import Mail from "./Mail";
+import Conect from "./Conect";
 
 const Contact = () => {
   return (
-    <div className="w-full flex flex-col items-center">
+    <div className="w-full flex flex-col items-center mx-10">
       <h1 className="font-bold text-white text-4xl md:text-5xl text-center w-full lg:max-w-[45vw]">
         Ready to take <span className="text-[#CBACF9]">your</span> digital
         presence to the next level?
@@ -14,9 +15,11 @@ const Contact = () => {
         your goals.
       </p>
 
-      <div className="flex w-full flex-col md:flex-row max-w-7xl gap-10 mx-auto">
-        <Profile></Profile>
+      <div className="flex w-full flex-col md:flex-row max-w-7xl gap-10 mx-auto justify-center items-center mt-10">
+        
         <Mail></Mail>
+        <Profile></Profile>
+        <Conect></Conect>
       </div>
     </div>
   );
--- components/Mail.tsx
+++ components/Mail.tsx
@@ -4,24 +4,56 @@ import { Icon } from "./ui/evervault-card";
 const Mail = () => {
   return (
     <>
-      <div className="border border-white/[0.2] flex flex-col items-start max-w-sm p-4 relative h-[20rem]">
+      <div className="flex-1 border border-white/[0.2] flex flex-col items-start max-w-sm p-4 relative h-[20rem]">
         <Icon className="absolute h-6 w-6 -top-3 -left-3 text-white" />
         <Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white" />
         <Icon className="absolute h-6 w-6 -top-3 -right-3 text-white" />
         <Icon className="absolute h-6 w-6 -bottom-3 -right-3 text-white" />
         {/* CARD  */}
-        <CardSpotlight className="">
-          <p className="text-xl font-bold relative z-20 mt-2 text-white">
+        <CardSpotlight className="flex flex-col justify-center items-center w-full">
+          <p className="text-2xl font-bold relative z-20 mt-2 text-white text-right w-full">
             Contact Information
           </p>
-          <div className="text-neutral-200 mt-4 relative z-20">
-            Follow these steps to secure your account:
-            
+          <div className="mt-5 gap-2 flex flex-col text-white-100 items-end z-20 relative">
+            <p className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5 mr-2 sm:mr-6"
+              >
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+              </svg>
+              <span>mdparvajmosharof17@gmail.com</span>
+            </p>
+            <p className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5 mr-2 sm:mr-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span>Uttara, Dhaka, Bangladesh</span>
+            </p>
+            <p className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5 mr-2 sm:mr-6"
+              >
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+              </svg>
+              <span>+880-1400139998</span>
+            </p>
           </div>
-          <p className="text-neutral-300 mt-4 relative z-20 text-sm">
-            Ensuring your account is properly secured helps protect your
-            personal information and data.
-          </p>
         </CardSpotlight>
       </div>
     </>
