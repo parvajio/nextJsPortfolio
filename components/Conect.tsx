7e1701a4e7f6ca217f6a80a50c354d59c88d8999
parvajio <mdparvajmosharof17@gmail.com>
2025-03-07 00:04:43 +0600

    contact info

---
+++ components/Conect.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Conect = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Conect
--- components/Mail.tsx
+++ components/Mail.tsx
@@ -1,16 +1,28 @@
+import { CardSpotlight } from "./ui/card-spotlight";
 import { Icon } from "./ui/evervault-card";
 
-
 const Mail = () => {
-
   return (
     <>
-      <div className="border border-white/[0.2] flex flex-col items-start max-w-sm p-4 relative h-[30rem]">
+      <div className="border border-white/[0.2] flex flex-col items-start max-w-sm p-4 relative h-[20rem]">
         <Icon className="absolute h-6 w-6 -top-3 -left-3 text-white" />
         <Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white" />
         <Icon className="absolute h-6 w-6 -top-3 -right-3 text-white" />
         <Icon className="absolute h-6 w-6 -bottom-3 -right-3 text-white" />
-        wow
+        {/* CARD  */}
+        <CardSpotlight className="">
+          <p className="text-xl font-bold relative z-20 mt-2 text-white">
+            Contact Information
+          </p>
+          <div className="text-neutral-200 mt-4 relative z-20">
+            Follow these steps to secure your account:
+            
+          </div>
+          <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+            Ensuring your account is properly secured helps protect your
+            personal information and data.
+          </p>
+        </CardSpotlight>
       </div>
     </>
   );
