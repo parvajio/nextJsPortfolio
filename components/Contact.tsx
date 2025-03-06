9217cc096938ba453ec1a0842d4417a553393d3a
parvajio <mdparvajmosharof17@gmail.com>
2025-03-06 22:53:42 +0600

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Profile } from './Profile'
-import Mail from './Mail'
+import React from "react";
+import { Profile } from "./Profile";
+import Mail from "./Mail";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="w-full flex flex-col items-center">
       <h1 className="font-bold text-white text-4xl md:text-5xl text-center w-full lg:max-w-[45vw]">
-          Ready to take <span className="text-[#CBACF9]">your</span> digital
-          presence to the next level?
-        </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
-        </p>
+        Ready to take <span className="text-[#CBACF9]">your</span> digital
+        presence to the next level?
+      </h1>
+      <p className="text-white-200 md:mt-10 my-5 text-center">
+        Reach out to me today and let&apos;s discuss how I can help you achieve
+        your goals.
+      </p>
 
+      <div className="flex w-full flex-col md:flex-row max-w-7xl gap-10 mx-auto">
         <Profile></Profile>
         <Mail></Mail>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -3,7 +3,7 @@ import { EvervaultCard, Icon } from "./ui/evervault-card";
 
 export function Profile() {
   return (
-    <div className="border border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+    <div className="border border-white/[0.2] flex flex-col items-start max-w-sm p-4 relative h-[30rem]">
       <Icon className="absolute h-6 w-6 -top-3 -left-3 text-white" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white" />
       <Icon className="absolute h-6 w-6 -top-3 -right-3 text-white" />
