5e1b0d23e7986d54432d1f86dea3b14949fcb966
parvajio <mdparvajmosharof17@gmail.com>
2025-03-07 21:52:32 +0600

    error fixing

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,18 @@ import Hero from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WhatIDO from "@/components/WhatIDO";
-// import Remote from "@/components/ui/Remote";
-import { isMobile } from "react-device-detect";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto">
       <Hero />
       <About />
-      {!isMobile && <Projects />}
+      <Projects />
       <Skills />
       <WhatIDO></WhatIDO>
       <Approach></Approach>
       <Contact></Contact>
       <Footer></Footer>
-      {/* <Remote /> */}
     </main>
   );
 }
--- components/ui/text-reveal-card.tsx
+++ components/ui/text-reveal-card.tsx
@@ -121,7 +121,6 @@ export const TextRevealCard = ({
             <p className="text-[4rem] md:text-[10rem] py-14 font-bold bg-clip-text text-transparent bg-[#9a9ac283]">
               {text}
             </p>
-            <MemoizedStars />
           </div>
         </div>
       </div>
@@ -154,42 +153,3 @@ export const TextRevealCardDescription = ({
     <p className={twMerge("text-[#a9a9a9] text-sm", className)}>{children}</p>
   );
 };
-
-const Stars = () => {
-  const randomMove = () => Math.random() * 4 - 2;
-  const randomOpacity = () => Math.random();
-  const random = () => Math.random();
-  return (
-    <div className="absolute inset-0">
-      {[...Array(80)].map((_, i) => (
-        <motion.span
-          key={`star-${i}`}
-          animate={{
-            top: `calc(${random() * 100}% + ${randomMove()}px)`,
-            left: `calc(${random() * 100}% + ${randomMove()}px)`,
-            opacity: randomOpacity(),
-            scale: [1, 1.2, 0],
-          }}
-          transition={{
-            duration: random() * 10 + 20,
-            repeat: Infinity,
-            ease: "linear",
-          }}
-          style={{
-            position: "absolute",
-            top: `${random() * 100}%`,
-            left: `${random() * 100}%`,
-            width: `2px`,
-            height: `2px`,
-            backgroundColor: "white",
-            borderRadius: "50%",
-            zIndex: 1,
-          }}
-          className="inline-block"
-        ></motion.span>
-      ))}
-    </div>
-  );
-};
-
-export const MemoizedStars = memo(Stars);
